101077a260445409ee28e33deb6f216328bbc962
Diyorbek Jumanov <76900500+djumanov@users.noreply.github.com>
2025-06-02 21:22:10 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from randomuser import randomuser_data
+
+def get_email_list(users: list[dict]) -> list[str]:
+    pass
+
+def get_name_list(users: list[dict]) -> list[str]:
+    pass
+
+def get_country_list(users: list[dict]) -> list[str]:
+    pass
+
+def get_phone_list(users: list[dict]) -> list[str]:
+    pass
+
+def get_city_list(users: list[dict]) -> list[str]:
+    pass
+
+def get_oldest_user(users: list[dict]) -> dict:
+    pass
+
+def get_youngest_user(users: list[dict]) -> dict:
+    pass
+
+def get_oldest_user(users: list[dict]) -> dict:
+    pass
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
