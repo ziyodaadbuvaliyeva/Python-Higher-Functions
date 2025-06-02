2d506f215db7eed82789dd094412f2c6f2ff370b
djumanov <djumanovdev@gmail.com>
2025-06-02 21:37:16 +0500

    email list

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from randomuser import randomuser_data
+from randomusers import randomusers_data
 
 def get_email_list(users: list[dict]) -> list[str]:
-    pass
+    emails = []
+    for user in users:
+        emails.append(user["email"])
+    return emails
 
 def get_name_list(users: list[dict]) -> list[str]:
     pass
@@ -21,11 +24,12 @@ def get_oldest_user(users: list[dict]) -> dict:
 def get_youngest_user(users: list[dict]) -> dict:
     pass
 
-def get_oldest_user(users: list[dict]) -> dict:
-    pass
 
 def main() -> None:
-    pass
+    results = randomusers_data["results"]
+
+    email_list = get_email_list(results)
+    print(email_list)
 
 if __name__ == "__main__":
     main()
