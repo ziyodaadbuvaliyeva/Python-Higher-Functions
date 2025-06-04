f6c54367dcebe67c520d841e7ccb38b8e6ad9a1f
ziyodaadbuvaliyeva <ziyodaadbuvaliyeva@gmail.com>
2025-06-04 17:13:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,28 +8,48 @@ def get_email_list(users: list[dict]) -> list[str]:
 
 def get_name_list(users: list[dict]) -> list[str]:
     pass
-
+names=[]
+for user in users:
+    full_name=f'{user['name']['first']}{user['name']['last']}'
+    names.append(full_name)
+    return names
 def get_country_list(users: list[dict]) -> list[str]:
     pass
-
+countries=[]
+for user in users:
+    countries.append(user['location']['country']
+                     return countries
 def get_phone_list(users: list[dict]) -> list[str]:
     pass
-
+phones=[]
+for user in users
+phones.append(user['phone'])
+return phones
 def get_city_list(users: list[dict]) -> list[str]:
     pass
+    cities=[]
+    for user in users:
+        cities.append(user['location']['city])
+                      return cities
 
 def get_oldest_user(users: list[dict]) -> dict:
     pass
-
+    oldest=users[0]
+for user in users:
+if user['date of birth']['age']>oldest['date of birth']['age]:
+oldest=user
+return oldest
 def get_youngest_user(users: list[dict]) -> dict:
     pass
-
+youngest=users[0]
+for user in users:
+if user['date of birth']['age']<youngest['date of birth']['age']:
+youngest=user
+return youngest
 
 def main() -> None:
-    results = randomusers_data["results"]
-
-    email_list = get_email_list(results)
-    print(email_list)
+    users=randomuser_data()
+    
 
 if __name__ == "__main__":
     main()
