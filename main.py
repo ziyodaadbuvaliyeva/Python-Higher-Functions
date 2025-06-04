1d6bbce435606c0168521ae962d0da7ac1f5fa8b
ziyodaadbuvaliyeva <ziyodaadbuvaliyeva@gmail.com>
2025-06-04 16:05:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,53 @@
-from randomusers import randomusers_data
+from randomuser import randomuser_data
 
 def get_email_list(users: list[dict]) -> list[str]:
-    emails = []
-    for user in users:
-        emails.append(user["email"])
+    pass
+emails=[]
+for user in users:
+    emails.append(user["email"])
     return emails
-
+    
 def get_name_list(users: list[dict]) -> list[str]:
     pass
-
+names[]
+for user in users:
+    full_name=f'{user['name']['first']}{user['name']['last']}'
+       names.append(full_name)
+    return names
+    
 def get_country_list(users: list[dict]) -> list[str]:
     pass
-
+country=[]
+for user in users:
+    countries.append(user[location']
+    ['country'])
+                     return countries
+                     
+                     
 def get_phone_list(users: list[dict]) -> list[str]:
     pass
-
+phones=[]
+for user in users:
+    phones.append(user['phone'])
+return phones
 def get_city_list(users: list[dict]) -> list[str]:
     pass
-
+cities=[]
+for user in users:
+    cities.append(user['location']
+                  ['city'])
+return cities
 def get_oldest_user(users: list[dict]) -> dict:
     pass
-
+return max(users,key=lambda user:user['age']['date])
 def get_youngest_user(users: list[dict]) -> dict:
     pass
-
-
+return min(users,key=lambda user:user['age']['date'])
+def get_oldest_user(users: list[dict]) -> dict:
+    pass
+return max(users,key=lambda user:user['date']['age'])
 def main() -> None:
-    results = randomusers_data["results"]
-
-    email_list = get_email_list(results)
-    print(email_list)
-
+    pass
+print('dastur  ishga tushdi!")
 if __name__ == "__main__":
     main()
